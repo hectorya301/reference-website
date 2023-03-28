40eb79736f2b12bd4b453e2c58a9ee6cc1b5f984
hectorya301 <126903019+hectorya301@users.noreply.github.com>
2023-03-28 17:04:23 +0000

    Upload

---
+++ object-intro.js
@@ -0,0 +1,63 @@
+/* When the script file is loaded, execution automatically begins at the top of the file */
+
+//console.log("Hello");
+
+// Player object information
+/*var player = {
+  name: "Nathan Drake",
+  health: 100,
+  stamina: 100,
+  inventory: "gun",
+  showInfo: function() {
+    console.log(this.name + " has " + this.health + " health and " + this.stamina + " stamina and has a " + this.inventory);
+  },
+  takeDamage: function() {
+    this.health -= 25;
+  }
+}
+
+// Give acces to player information
+player.showInfo();
+player.health = 50;
+player.showInfo();
+player.takeDamage();
+player.showInfo();
+console.log(player["inventory"]);
+console.log(player.inventory);*/
+
+/* Practice Problem 1
+var pizza = {
+  name: "pizza",
+  size: "large",
+  toppings: "pepperoni and jalapeno",
+  price: "$11.95",
+  showInfo: function() {
+    console.log(" This " + this.name + " is a size " + this.size + " with the toppings " + this.toppings + " for the price of " + this.price);
+  },
+}
+
+pizza.showInfo();
+pizza.size = "medium";
+pizza.showInfo();
+pizza.toppings = "pepperoni, green peppers, and pineapple";
+pizza.showInfo();*/
+
+/* Practice Problem 2
+var student = {
+  firstName: "Hector",
+  lastName: "Yanez",
+  age: "18",
+  grade: "12th",
+  matric: "0516527001",
+  showName: function() {
+    console.log(" The name of this student is " + this.firstName + " " + this.lastName);
+  },
+  showInfo: function() {
+    console.log(this.firstName + " " + this.lastName + ", " + "age: " + this.age + ", grade: " + this.grade + ", matric: " + this.matric);
+  },
+}
+
+student.showName();
+student.showInfo();
+student.age = "10";
+student.showInfo();*/
